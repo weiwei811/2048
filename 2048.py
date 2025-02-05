22537d53084463e470373f6e764be4963e7be3be
weiwei811 <weiwei.huang811@gmail.com>
2025-02-05 15:52:28 +0800

    finish death/score/drawtop

--- 2048.py
+++ 2048.py
@@ -1,33 +1,32 @@
 import pygame
 import random
-pygame.init()
+from os.path import isfile
 
-# draw  different color
-# addblock 
-# death
-# score 
+pygame.init()
 
 def move_left(board):
 	b=[]
+	s=0
 	for line in board:
 		newline=[x for x in line if x!=0]
 		for i in range(len(newline)-1):
 			if newline[i]==newline[i+1]:
 				newline[i]*=2
+				s+=newline[i]
 				newline[i+1]=0
 		newline=[x for x in newline if x!=0]
 		# print("newline",newline)
 		line=newline+[0]*(4-len(newline))
 		b.append(line)
-	return b
+	return b,s
 
 def move(board,direction):
 	if direction==0: #left
-		board=move_left(board)
+		board,s=move_left(board)
 	elif direction==2: #right
 		for i in range(4):
 			board[i]=board[i][::-1]
-		board=move_left(board)
+		board,s=move_left(board)
 		for i in range(4):
 			board[i]=board[i][::-1]
 	elif direction==1: #up
@@ -35,7 +34,7 @@ def move(board,direction):
 		for i in range(4):
 			for j in range(4):
 				board2[j][i]=board[i][j]
-		board2=move_left(board2)
+		board2,s=move_left(board2)
 		for i in range(4):
 			for j in range(4):
 				board[j][i]=board2[i][j]
@@ -46,14 +45,15 @@ def move(board,direction):
 				board2[j][i]=board[i][j]
 		for i in range(4):
 			board2[i]=board2[i][::-1]
-		board2=move_left(board2)
+		board2,s=move_left(board2)
 		for i in range(4):
 			board2[i]=board2[i][::-1]
 		for i in range(4):
 			for j in range(4):
 				board[j][i]=board2[i][j]
-	board=add_block(board)
-	return board
+	if not death(board):
+		board=add_block(board)
+	return board,s
 
 def add_block(board,n=1):
 	empty=[]
@@ -66,55 +66,97 @@ def add_block(board,n=1):
 		board[x][y]=random.choices([2,4],weights=[0.7,0.3])[0]
 	return board
 
-def draw(screen,board):
+def drawtile(screen,board):
 	for i in range(4):
 		for j in range(4):
-			pygame.draw.rect(screen,(153,153,255),(stripsize*(i+1)+tilesize*i,stripsize*(j+1)+tilesize*j,tilesize,tilesize))
+			pygame.draw.rect(screen,(153,153,255),(stripsize*(i+1)+tilesize*i,stripsize*(j+1)+tilesize*j+topsize,tilesize,tilesize))
 			if board[i][j]!=0:
 				text=font.render(str(board[i][j]),True,(0,0,0))
-				text_rect = text.get_rect(center=(stripsize*(i+1)+tilesize*i+tilesize//2,stripsize*(j+1)+tilesize*j+tilesize//2))
+				text_rect = text.get_rect(center=(stripsize*(i+1)+tilesize*i+tilesize//2,stripsize*(j+1)+tilesize*j+tilesize//2+topsize))
 				screen.blit(text, text_rect)
 
-def init():
+def drawtop(score):
+	text=big_font.render("2048",True,(0,0,0))
+	screen.blit(text, (20,20))
+
+
+	center=(400,50)
+	rectheight=70
+	padding=10
+	w,h=font.size(str(score))
+	pygame.draw.rect(screen,(234,231,218),(center[0]-(w+2*padding)/2,center[1]-rectheight/2,w+2*padding,rectheight))
+	
+	scoretext=font.render(str(score),True,(0,0,0))
+	text_rect = scoretext.get_rect(center=center)
+	screen.blit(scoretext, text_rect)
+
+
+
+def init(d=False):
+	if d:
+		return [[1,2,3,4],[5,6,7,8],[9,1,2,3],[1,2,3,3]]
 	board=[[0 for i in range(4)] for j in range(4)]
 	board=add_block(board,2)
 	print(board)
 	return board
 
 def death(board):
-	#0 check
-	
-	#same
-	for i in range(4)
+	for i in range(4):
+		for j in range(4):
+			if board[i][j]==0: #0 check
+				return False
+			if j!=3: #right check
+				if board[i][j]==board[i][j+1]:
+					return False
+			if i!=3: #down check
+				if board[i][j]==board[i+1][j]:
+					return False
+	return True
+
+def draw_death():
+	pass
+
+topsize=100
 tilesize=150
 stripsize=20
-height=4*tilesize+5*stripsize
+height=4*tilesize+5*stripsize+topsize
 width=4*tilesize+5*stripsize
 screen=pygame.display.set_mode((width,height))
 pygame.display.set_caption("2048")
 clock=pygame.time.Clock()
 board=init()
+score=0
 font=pygame.font.Font(None,36)
+big_font=pygame.font.SysFont("gurmukhi",80)
 running=True
 
+if not isfile("score.txt"):
+	with open("score.txt","w") as f:
+		f.write('0')
+with open("score.txt","r") as f:
+	hs=int(f.read())
+
+m={pygame.K_w:0,pygame.K_UP:0,pygame.K_s:2,pygame.K_DOWN:2,pygame.K_a:1,pygame.K_LEFT:1,pygame.K_d:3,pygame.K_RIGHT:3}
+
 while running:
-	screen.fill((255, 230, 204))
+	screen.fill((250,248,240))
 	#motion
 	for event in pygame.event.get():
 		if event.type==pygame.KEYDOWN:
-			if event.key==pygame.K_w or event.key ==pygame.K_UP:
-				board=move(board,0)
-			elif event.key==pygame.K_s or event.key ==pygame.K_DOWN:
-				board=move(board,2)
-			elif event.key==pygame.K_a or event.key ==pygame.K_LEFT:
-				board=move(board,1)
-			elif event.key==pygame.K_d or event.key ==pygame.K_RIGHT:
-				board=move(board,3)
+			if event.key in m.keys():
+				board,s=move(board,m[event.key])
+				score+=s
 
 		elif event.type==pygame.QUIT:
 			running=False
 
-	draw(screen,board)
-
+	drawtile(screen,board)
+	drawtop(score)
+	if death(board):
+		draw_death()
+		if score>hs:
+			with open('score.txt',"w") as f:
+				hs=score
+				f.write(str(hs))
 	pygame.display.flip()
 	clock.tick(60)
